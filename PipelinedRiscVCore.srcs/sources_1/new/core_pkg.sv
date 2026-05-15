8e1e3da4ac6092063c56059315eb3f5729ec1c07
YogithL <lyogith@gmail.com>
2026-05-14 22:19:41 -0500

    Polished ALU from MultiCycle, modified RegFile for pipelining, took ImmGen from multicycle, created Branch Control Unit

--- PipelinedRiscVCore.srcs/sources_1/new/core_pkg.sv
+++ PipelinedRiscVCore.srcs/sources_1/new/core_pkg.sv
@@ -6,6 +6,12 @@ typedef enum logic
     REGB = 1'b1  
 } alusrc_e;
 
+typedef enum logic
+{
+    BRANCH = 1'b00, 
+    PCPLUS4 = 1'b01
+} pc_sel_e;
+
 typedef enum logic[1:0]
 {
     NO_FORWARD  = 2'b00, 
@@ -13,9 +19,16 @@ typedef enum logic[1:0]
     FORWARD_WB  = 2'b10
 } forward_sel_e;
 
-typedef enum logic[3:0]      
+typedef enum logic[1:0]
+{
+    MEM = 2'b00,
+    ALU =  2'b01,
+    PC4 = 2'b10
+} resultsrc_e;
+
+typedef enum logic[2:0]      
 {                            
-    //MSB appended with opcode bit7
+    //MSB appended with bit30
     // Arithmetic            
     ALU_ADD = 4'b0000,       
     ALU_SUB = 4'b1000,       
@@ -28,19 +41,58 @@ typedef enum logic[3:0]
     // Shifts                
     ALU_SRL = 4'b0101,       
     ALU_SRA = 4'b1101,       
-                         
-    // More Logical          
     ALU_SLL = 4'b0001,       
-    ALU_SLT = 4'b0010,       
-    ALU_SLTU = 4'b0011       
-} alu_opps_e;                  
+                         
+    // Branch Focused
+    ALU_SLT = 4'b0010,     
+    ALU_SLTU = 4'b0011
+} alu_opps_e;
 
-typedef enum logic[1:0]
+typedef enum logic[2:0]
 {
-    MEM = 2'b00,
-    ALU =  2'b01,
-    PC4 = 2'b10
-} resultsrc_e;
+    BR_BEQ  = 3'b000, //Branch if equal                          
+    BR_BNE  = 3'b001, //Branch if not equal                      
+    BR_BLT  = 3'b100, //Branch if less than (Signed)             
+    BR_BGE  = 3'b101, //Branch if greater or equal (Signed)      
+    BR_BLTU = 3'b110, //Branch if less than (Unsigned)           
+    BR_BGEU = 3'b111  //Branch if greater or equal (Unsigned)    
+} branch_types_e;
+
+typedef enum logic[6:0]
+{
+    //R-Type (1)              
+    OP_Reg = 7'b0110011,       
+                           
+    //I-Type (2)              
+    OP_Imm = 7'b0010011,                
+    //I-Type (2)              
+    OP_Load = 7'b0000011,   
+    //I-Type (2)              
+    OP_JALR = 7'b1100111,      
+                           
+    //S-Type (3)              
+    OP_Store = 7'b0100011,     
+                           
+    //B-Type (4)              
+    OP_Branch = 7'b1100011,     
+                           
+    //U-Type (5)              
+    OP_LUI = 7'b0110111,                    
+    //U-Type (5)              
+    OP_AUIPC = 7'b0010111,     
+                           
+    //J-Type (6)              
+    OP_JAL = 7'b1101111        
+} opcodes_e;    
+
+typedef enum logic [2:0]
+{
+    IMM_I = 3'b000,
+    IMM_S = 3'b001,
+    IMM_B = 3'b010,
+    IMM_J = 3'b011,
+    IMM_U = 3'b100 
+} immsrc_e;
 
 typedef enum logic[2:0]
 {
@@ -82,6 +134,7 @@ typedef struct packed
     alu_opps_e ALUControlE;
     logic JumpE;
     logic BranchE;
+    branch_types_e BranchType;
     width_e SizeE;
 } p_id_ex_s;
 
---
+++ PipelinedRiscVCore.srcs/sources_1/new/riscv_units.sv
@@ -0,0 +1,149 @@
+import riscV_pkg::*;
+
+module ALU(
+    input logic[31:0] A,
+    input logic[31:0] B,
+    input alu_opps_e ALUControl, //Append funct3 with bit30
+    output logic[31:0] ALUResult,
+    output logic[3:0] NZVC
+    );
+    
+    always_comb begin
+        ALUResult = 32'b0;
+        NZVC = 4'b0000;
+        
+        case(ALUControl)
+            ALU_ADD: ALUResult = A + B;
+            ALU_SUB: ALUResult = A - B;
+            
+            ALU_OR: ALUResult = A | B;
+            ALU_AND: ALUResult = A & B;
+            ALU_XOR: ALUResult = A ^ B;
+            
+            ALU_SRL: ALUResult = A >> B[4:0];
+            ALU_SRA: ALUResult = $signed(A) >>> B[4:0];
+            
+            ALU_SLL: ALUResult = A << B[4:0];
+            
+            ALU_SLT: ALUResult = $signed(A) < $signed(B) ? 32'h0001 : 32'h0000;
+            ALU_SLTU: ALUResult = (A) < (B) ? 32'h0001 : 32'h0000;
+            
+            default: ALUResult = 32'b0;
+        endcase
+         
+        NZVC[1] = ~(| ALUResult);
+        
+        NZVC[0] = ALUResult[31]; 
+        
+        if(ALUControl == ALU_ADD) begin
+            NZVC[2] = (A[31] == B[31]) && (A[31] != ALUResult[31]) ? 1'b1 : 1'b0;
+            NZVC[3] = ({1'b0, A} + {1'b0, B} > {1'b0, ALUResult}) ? 1'b1 : 1'b0; 
+        end
+        
+        if(ALUControl == ALU_SUB) begin
+            NZVC[2] = (A[31] != B[31]) && (A[31] != ALUResult[31]) ? 1'b1 : 1'b0;
+            NZVC[3] = B > A  ? 1'b1 : 1'b0; 
+        end
+    end
+    
+endmodule
+
+
+
+module RegFile(
+    input logic clk,
+    input logic RegWrite,
+    input logic[4:0] readAddr1,
+    input logic[4:0] readAddr2,
+    input logic[4:0] writeAddr,
+    input logic[31:0] writeData,
+    output logic[31:0] readData1,
+    output logic[31:0] readData2
+    );
+    
+    logic[31:0] RegArray[0:31];
+    
+    always_ff @(posedge clk) begin
+        if(RegWrite && (writeAddr != 5'b0))
+            RegArray[writeAddr] <= writeData;    
+    end
+    
+    always_comb begin
+        if(readAddr1 == 5'b0)
+            readData1 = 32'b0;
+        else if(readAddr1 == writeAddr && RegWrite)
+            readData1 = writeData;
+        else
+            readData1 = RegArray[readAddr1];
+        
+        if(readAddr2 == 5'b0)
+            readData2 = 32'b0;
+        else if(readAddr2 == writeAddr && RegWrite)
+            readData2 = writeData;
+        else
+            readData2 = RegArray[readAddr2];
+    end
+endmodule
+
+module ImmGen(
+    input logic[31:0] instr,
+    input immsrc_e immControl,
+    output logic[31:0] imm
+    );
+        
+    always_comb begin        
+        case(immControl)
+            IMM_I: imm = { {20{instr[31]}} , instr[31:20] };
+            IMM_U: imm = { instr[31:12] , 12'b0 };
+            IMM_S: imm = { {20{instr[31]}} , instr[31:25] , instr[11:7] };
+            IMM_B: imm = { {19{instr[31]}} , instr[31] , instr[7] , instr[30:25] , instr[11:8] , 1'b0 };
+            IMM_J: imm = { {11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0 };
+            default: imm = 32'b0;
+        endcase
+    end
+        
+endmodule       
+
+
+
+module BranchControlUnit(
+    input logic[3:0] NZVC,
+    input logic branchEnable,
+    input logic jumpEnable,
+    input branch_types_e branchType,
+    output pc_sel_e PCSelection
+    );
+    
+    logic takeBranch;
+    
+    assign PCSelection = (branchEnable && takeBranch) || jumpEnable ? BRANCH : PCPLUS4;
+    
+    always_comb begin
+        takeBranch = 1'b0;
+        
+        case(branchType)
+            BR_BEQ: takeBranch = NZVC[1];
+            BR_BNE: takeBranch = ~NZVC[1];
+            BR_BLT: takeBranch = NZVC[0] ^ NZVC[2];
+            BR_BLTU: takeBranch = NZVC[3];
+            BR_BGE: takeBranch = NZVC[1] || ~(NZVC[0] ^ NZVC[2]);
+            BR_BGEU: takeBranch = NZVC[1] || ~NZVC[3];
+            default: takeBranch = 1'b0;
+        endcase
+    end
+    
+endmodule           
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
