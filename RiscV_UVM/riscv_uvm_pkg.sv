dcc36ed85fc948f32c1b2ad9aae4d11f88821202
YogithL <lyogith@gmail.com>
2026-06-27 22:29:30 -0500

    Resolved all issues from switching to WSL and creating useless spike model, back to windows. Need to create golden model.

--- RiscV_UVM/riscv_uvm_pkg.sv
+++ RiscV_UVM/riscv_uvm_pkg.sv
@@ -2,14 +2,7 @@ package riscv_uvm_pkg;
 
     import uvm_pkg::*;
     `include "uvm_macros.svh"
-
-    import "DPI-C" context function void spike_init();
-    import "DPI-C" context function void spike_reset();
-    import "DPI-C" context function void spike_step(int instr);
-    import "DPI-C" context function int  spike_get_reg(int reg_idx);
-    import "DPI-C" context function void spike_write_mem(int addr, int data);
-    import "DPI-C" context function void spike_set_pc(int val);
-
+    
     `include "base_stim_packet.sv"
     `include "mem_packet.sv"
     `include "pc_packet.sv"
